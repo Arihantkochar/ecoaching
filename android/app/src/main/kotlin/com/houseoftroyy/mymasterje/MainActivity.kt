347252d6d67d01ecf2247ab99f114fc3026f4338
Arihant Kochar <arihantkochar@Arihants-MacBook-Pro.local>
2021-05-10 15:52:52 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/houseoftroyy/mymasterje/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.houseoftroyy.mymasterje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/styles/themes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  primaryColor: Color(0xFF7D82B8),
+  primaryColorLight: Color(0xFFC3DFE0),
+  primaryColorDark: Color(0xFF003844),
+  accentColor: Color(0xFFE8998D),
+  shadowColor: Color(0xFF000000).withOpacity(0.3),
+  splashColor: Color(0xFFFFFFFF),
+);
